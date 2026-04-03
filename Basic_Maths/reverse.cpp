51ad077c211b3bc0e6388b6eb809625eaccba9a4
Shruti Rai <shrutiraii.rnc@gmail.com>
2026-04-03 22:43:50 +0530

    Reverse integers

---
+++ Basic_Maths/reverse.cpp
@@ -0,0 +1,24 @@
+Leetcode - Reverse integer
+Difficulty - medium
+https://leetcode.com/problems/reverse-integer/description/
+
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+
+            // Check overflow before multiplying
+            if (rev > INT_MAX / 10 || rev < INT_MIN / 10) {
+                return 0;
+            }
+
+            rev = rev * 10 + digit;
+        }
+
+        return rev;
+    }
+};
