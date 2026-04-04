5ec426d84b2160ff58a294fd90e812487c93f327
Shruti Rai <shrutiraii.rnc@gmail.com>
2026-04-04 23:46:02 +0530

    Finding Union of 2 Sorted Arrays

---
+++ Arrays/Union.cpp
@@ -0,0 +1,46 @@
+GFG - Find Union of 2 Sorted arrays
+Difficulty - Medium
+https://www.geeksforgeeks.org/problems/union-of-two-sorted-arrays-1587115621/1
+
+class Solution {
+  public:
+        // code here
+   vector<int> findUnion(vector<int> a, vector<int> b) {
+    int n1 = a.size();
+    int n2 = b.size();
+    int i = 0;
+    int j = 0;
+    vector<int> unionArr;
+
+    while(i < n1 && j < n2) {
+        if(a[i] <= b[j]) {
+            if(unionArr.size() == 0 || unionArr.back() != a[i]) {
+                unionArr.push_back(a[i]);
+            }
+            i++;
+        } else {
+            if(unionArr.size() == 0 || unionArr.back() != b[j]) {
+                unionArr.push_back(b[j]);
+            }
+            j++;
+        }
+    }
+
+    while(j < n2) {
+        if(unionArr.size() == 0 || unionArr.back() != b[j]) {
+            unionArr.push_back(b[j]);
+        }
+        j++;
+    }
+
+    while(i < n1) {
+        if(unionArr.size() == 0 || unionArr.back() != a[i]) {
+            unionArr.push_back(a[i]);
+        }
+        i++;
+    }
+
+    return unionArr;
+}
+  };
+  
