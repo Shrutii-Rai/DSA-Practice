65c2575128d1c7e4eccbc307ac4923edcd4f2a45
Shruti Rai <shrutiraii.rnc@gmail.com>
2026-04-05 22:41:25 +0530

    Robot return  to origin question

---
+++ Strings/robotReturnto_origin.cpp
@@ -0,0 +1,17 @@
+Leetcode - Robot return to origin
+Difficulty - Easy
+https://leetcode.com/problems/robot-return-to-origin/description/
+
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0, y = 0;
+        for (char move : moves) {
+            if (move == 'U') y++;
+            else if (move == 'D') y--;
+            else if (move == 'R') x++;
+            else if (move == 'L') x--;
+        }
+        return x == 0 && y == 0;
+    }
+};
