adb596ad0c8cb217779b9c7872736b77f1181101
Shruti <shrutiraii.rnc@gmail.com>
2026-06-16 19:41:27 +0530

    Add DSA Practice solutions

---
+++ Arrays/Leetcode -.cpp
@@ -0,0 +1,32 @@
+Leetcode - Process String with special operators
+Difficulty - medium
+https:https://leetcode.com/problems/process-string-with-special-operations-i/description/?envType=daily-question&envId=2026-06-16
+
+class Solution {
+public:
+    string processStr(string s) {
+        string res;
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            char ch = s[i];
+
+            if (ch == '*') {
+                if (res.length() != 0) {
+                    res.pop_back();
+                }
+            } 
+            else if (ch == '#') {
+                res += res;
+            } 
+            else if (ch == '%') {
+                reverse(res.begin(), res.end());
+            } 
+            else if (ch >= 'a' && ch <= 'z') {
+                res.push_back(ch);
+            }
+        }
+
+        return res;
+    }
+};
